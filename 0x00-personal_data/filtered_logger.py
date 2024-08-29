4d4d97b5b15c9ae95bef990b176d44c2e55109b6
MGHefny <mgenedhefny@gmail.com>
2024-08-29 16:54:10 +0300

    0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ import re
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+                 message: str, separator: str):
     """ filter datum message """
     for x in fields:
         message = re.sub(f'{x}=.*?{separator}',
