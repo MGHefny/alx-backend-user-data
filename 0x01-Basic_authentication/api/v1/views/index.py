a61986de9d82f6349e66a7b9e343b1d4d84761ce
MGHefny <mgenedhefny@gmail.com>
2024-09-02 19:47:03 +0300

    1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'])
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - abort error 401
+    """
+    abort(401)
