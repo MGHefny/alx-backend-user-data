df7fe8a0064cbe628a8cd46efb90b71eca243581
MGHefny <mgenedhefny@gmail.com>
2024-09-03 16:37:02 +0300

    7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 """ model basic auth
 """
+import base64
 from .auth import Auth
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
     """ cls basic auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ base 64 authorize
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        access = authorization_header.split(" ")[-1]
+        return access
