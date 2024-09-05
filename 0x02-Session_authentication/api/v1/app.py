18031b8b20e0d199d1595cbc5af1663fee9e9c1f
MGHefny <mgenedhefny@gmail.com>
2024-09-05 21:32:30 +0300

    adv1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 import os
 
 
@@ -23,6 +24,8 @@ if auth_t == 'basic_auth':
     auth = BasicAuth()
 if auth_t == 'session_auth':
     auth = SessionAuth()
+if auth_t == 'session_exp_auth':
+    auth = SessionExpAuth()
 
 
 @app.errorhandler(404)
