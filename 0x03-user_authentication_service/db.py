72976399eeb22b9b193642dec74488f53e5be6d9
MGHefny <mgenedhefny@gmail.com>
2024-09-12 02:58:40 +0300

    1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,9 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-from typing import TypeVar
+
 from user import Base, User
 
 
@@ -34,7 +32,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """
         """
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
+        n_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(n_user)
         self._session.commit()
-        return user
+        return n_user
