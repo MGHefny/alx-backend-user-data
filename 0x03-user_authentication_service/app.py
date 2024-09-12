bc69ed3e8d58fdbbc6fb970fdd1fe0912a5c8dee
MGHefny <mgenedhefny@gmail.com>
2024-09-12 05:41:40 +0300

    login/logout session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ app flask
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ def home() -> str:
 
 
 @app.route("/users", methods=["POST"], strict_slashes=False)
-def c_user() -> str:
+def users() -> str:
     """ user post
     """
     email = request.form.get("email")
@@ -26,6 +26,33 @@ def c_user() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """ start log
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    session_id = AUTH.create_session(email)
+    reply = jsonify({"email": email, "message": "logged in"})
+    reply.set_cookie("session_id", session_id)
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    else:
+        return reply
+
+
+@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+def logout() -> str:
+    """ end log
+    """
+    session_id = request.cookies.get("session_id")
+    u = AUTH.get_user_from_session_id(session_id)
+    if u is None:
+        abort(403)
+    AUTH.destroy_session(u.id)
+    return redirect("/")
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
-""" auth model"""
+""" auth model
+"""
 import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 from user import User
 
 
 def _hash_password(password: str) -> str:
-    """ hash pass"""
+    """ hash pass
+    """
     x = password.encode("utf-8")
     y = bcrypt.gensalt()
     encript_paw = bcrypt.hashpw(x, y)
     return encript_paw
+
+
+def _generate_uuid() -> str:
+    """ uuid generate
+    """
+    UUID = uuid4()
+    return str(UUID)
 
 
 class Auth:
@@ -22,7 +32,8 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """ login user """
+        """ login user
+        """
         x = self._db
         try:
             u = x.find_user_by(email=email)
@@ -32,3 +43,35 @@ class Auth:
             return u
         else:
             raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ login valid
+        """
+        x = self._db
+        try:
+            u = x.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        u_paw = u.hashed_password
+        hash_paw = password.encode("utf-8")
+
+        if bcrypt.checkpw(hash_paw, u_paw):
+            return True
+
+        return False
+
+    def create_session(self, email: str) -> str:
+        """ new session
+        """
+        x = self._db
+        try:
+            u = x.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+
+        x.update_user(u.id, session_id=session_id)
+
+        return session_id
