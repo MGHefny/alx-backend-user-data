bcba569562fa29c5ae26030e244e601f7da2164a
MGHefny <mgenedhefny@gmail.com>
2024-09-12 04:47:07 +0300

    5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ auth model"""
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password: str) -> str:
@@ -9,3 +12,22 @@ def _hash_password(password: str) -> str:
     y = bcrypt.gensalt()
     encript_paw = bcrypt.hashpw(x, y)
     return encript_paw
+
+class Auth:
+    """ auth cls
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ login user """
+        x = self._db
+        try:
+            u = x.find_user_by(email=email)
+        except NoResultFound:
+            hash_paw = _hash_password(password)
+            u = x.add_user(email, hash_paw)
+            return u
+        else:
+            raise ValueError(f'User {email} already exists')
