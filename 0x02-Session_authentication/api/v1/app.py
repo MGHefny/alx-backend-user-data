6be02f743dc8cad0322482891242be558d53ae8e
MGHefny <mgenedhefny@gmail.com>
2024-09-12 19:51:43 +0300

    try

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,6 +2,7 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -9,7 +10,6 @@ from flask_cors import (CORS, cross_origin)
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
-import os
 
 
 app = Flask(__name__)
